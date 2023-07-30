51b9680458351be1547e17fe91b3a35b658462eb
Hakim Saoudi <68503582+saoudi-h@users.noreply.github.com>
2023-07-30 22:33:27 +0200

    Add the form functions to the details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housinglocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   template: `
     <article>
       <img class="listing-photo" [src]="housingLocation?.photo"
@@ -28,14 +32,32 @@ import { HousingLocation } from '../housinglocation';
   `,
   styleUrls: ['./details.component.css']
 })
+
+
+
+
 export class DetailsComponent {
 
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
   }
 }
